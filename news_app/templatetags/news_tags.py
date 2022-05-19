bd690bb1c6fefea84541515d74910f49982f1231
kingco6ra <king.co6ra@yandex.ru>
2022-05-19 10:44:43 +0300

    delete cache

--- news_app/templatetags/news_tags.py
+++ news_app/templatetags/news_tags.py
@@ -1,7 +1,6 @@
 from django import template
 from django.db.models import Count
 from news_app.models import Category
-from django.core.cache import cache
 
 # регистрация template тэга
 register = template.Library()
@@ -9,8 +8,5 @@ register = template.Library()
 
 @register.inclusion_tag('list_categories.html')
 def show_categories():
-    categories = cache.get('categories')
-    if not categories:
-        categories = Category.objects.all().annotate(cnt=Count('news')).filter(cnt__gt=0)
-        cache.set('categories', categories, 30)
+    categories = Category.objects.all().annotate(cnt=Count('news_category')).filter(cnt__gt=0)
     return {'categories': categories}
