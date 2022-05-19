486cc6cc5c3a8d274d7e9d6204a426a5e4554f3d
kingco6ra <king.co6ra@yandex.ru>
2022-05-19 09:50:17 +0300

    format datetimefield

--- news_app/serializers.py
+++ news_app/serializers.py
@@ -5,6 +5,7 @@ from .models import News, Comments
 
 class NewsSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    created_at = serializers.DateTimeField(read_only=True, format='%H:%M:%S %Y-%m-%d')
 
     class Meta:
         model = News
@@ -13,6 +14,7 @@ class NewsSerializer(serializers.ModelSerializer):
 
 class CommentsSerializer(serializers.ModelSerializer):
     news = serializers.HyperlinkedRelatedField(view_name='news-detail', queryset=News.objects.all())
+    created_at = serializers.DateTimeField(read_only=True, format='%H:%M:%S %Y-%m-%d')
 
     class Meta:
         model = Comments
