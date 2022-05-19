28857d84e2b22a391504f2fe49419876be72aed6
kingco6ra <king.co6ra@yandex.ru>
2022-05-19 22:10:25 +0300

    reduction of pagination for example

--- news_project/settings.py
+++ news_project/settings.py
@@ -247,7 +247,7 @@ CAPTCHA_NOISE_FUNCTIONS = None
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 5,
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
