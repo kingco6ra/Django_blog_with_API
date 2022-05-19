39af520e12d78327be3e147695ab6f4df218efd6
kingco6ra <king.co6ra@yandex.ru>
2022-05-19 14:06:22 +0300

    new permission

--- news_app/permissions.py
+++ news_app/permissions.py
@@ -1,9 +1,17 @@
-from rest_framework.permissions import IsAdminUser, SAFE_METHODS
+from rest_framework.permissions import IsAdminUser, SAFE_METHODS, BasePermission
 
 
 class IsAdminOrReadOnly(IsAdminUser):
-
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
+
+
+class IsOwnerOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
