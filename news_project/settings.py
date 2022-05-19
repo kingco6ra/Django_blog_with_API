4c8f66f6e5c587885908936898f9b1bd6bf2051a
kingco6ra <king.co6ra@yandex.ru>
2022-05-19 19:53:21 +0300

    deleted unnecessary import

--- news_project/settings.py
+++ news_project/settings.py
@@ -13,8 +13,6 @@ import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import dj_database_url
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -109,8 +107,6 @@ DATABASES = {
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
